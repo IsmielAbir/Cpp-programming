78ed6f7824b1eb792841b09606c6f74fb495a872
Ismielabir <ismielabir286@gmail.com>
2022-07-14 12:28:49 +0600

    first commit

---
+++ 2D_Matrix.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter your row and column: ";
+    cin>>n>>m;
+    int a[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
+
+    cout<<"Your 2D Matrix is\n";
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
