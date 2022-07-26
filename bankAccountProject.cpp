6e0e4a736d61760dda23d0aa6afe43619fbd7ac5
Ismielabir <ismielabir286@gmail.com>
2022-07-26 14:22:43 +0600

    Endless Coding

--- bankAccountProject.cpp
+++ bankAccountProject.cpp
@@ -15,24 +15,38 @@ class BankAccount{
 
     BankAccount(string account_holder, string address, int age, string password){
         this->account_holder=account_holder;
-        this->address=adress;
+        this->address=address;
         this->age=age;
         this->password=password;
         this->account_number=rand()%1000000000;
+        this->balance=0;
         cout<<"Your account no is "<<this->account_number<<endl;
+    }
+
+    int show_balance(string password){
+        if(this->password==password)
+        return this->balance;
+        else
+        return -1;
     }
 
 };
 
-void create_account(){
+BankAccount* create_account(){
     string account_holder, password, address;
     int age;
     cout<<"Create Account "<<endl;
     cin>>account_holder>>address>>age>>password;
+    BankAccount *myAccount=new BankAccount(account_holder, address, age, password);
+    return myAccount;
 }
 
 int main(){
-
+    BankAccount *myAccount = create_account();
+    if(myAccount->show_balance("abc")==-1)
+    cout<<"Password did not match";
+    else
+    cout<<"Your bank balance is "<<myAccount->show_balance("abc")<<endl;
 
     return 0;
 }
